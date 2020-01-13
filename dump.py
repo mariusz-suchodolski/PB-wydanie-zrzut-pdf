57cb04d29fb15a2d2cfe31de29d92f8e2f52f524
mariusz-suchodolski <mariusz@suchodol.ski>
2020-01-13 15:41:07 +0100

    Updated for Python 3 Swapped img2pdf to FPDF Added automatic download of latest release of Puls Bizensu

--- dump.py
+++ dump.py
@@ -3,25 +3,48 @@ import re
 import os
 from io import BytesIO
 import requests
-import img2pdf
+from fpdf import FPDF
+import sys
+import smtplib
 
 s = requests.Session()
 pat = r"<input id=\"csrf_token\" name=\"csrf_token\" type=\"hidden\" value=\"(.{40})\">"
-csrftokenget = s.get("https://www.pb.pl/konto/logowanie/").content
+csrftokenget = s.get("https://www.pb.pl/konto/logowanie/").content.decode("utf-8")
 csrftoken = re.findall(pat, csrftokenget)[0]
-print "Znaleziono token CSRF: " + csrftoken
+print("Znaleziono token CSRF: " + csrftoken)
 s.get("https://www.pb.pl/konto/logowanie/")
 s.post("https://www.pb.pl/konto/logowanie/", data={'csrf_token': csrftoken,
-                                                   'email': '',
-                                                   'password': ''})
+                                                   'email': os.environ['PB_LOGIN'],
+                                                   'password': os.environ['PB_PASS']})
 
-numer = input("Podaj numer wydania: ")
-rok = input("Podaj rok wydania: ")
+numer = 0
+rok = 0
+try:
+    print(sys.argv[1])
+except IndexError:
+    numer = input("Podaj numer wydania: ")
+    rok = input("Podaj rok wydania: ")
+else:
+    if sys.argv[1] == "--latest":
+        print("Pobieram najnowsze wydanie")
+        pat = r"<p class=\"issue\">Nr (.{6,8})</p>"
+        wydanieget = s.get("https://www.pb.pl/konto/e-wydania/").content.decode("utf-8")
+        wydanie = re.findall(pat, wydanieget)[0]
+        numer = wydanie.split("/")[0]
+        rok = wydanie.split("/")[1]
+    else:
+        quit()
+
+resp = s.get('https://www.pb.pl/konto/e-wydanie/' + str(rok) + '/pb' + numer.zfill(3) + '/1.png')
+bites = BytesIO(base64.b64decode(resp.content))
+f_name = "okladka pulsbiznesu " + numer.zfill(3) + " " + str(rok) + ".png"
+with open(f_name, 'wb') as f:
+    f.write(resp.content)
 
 x = 1
 eof = 0
 while eof == 0:
-    resp = s.get('https://www.pb.pl/konto/e-wydanie/'+str(rok)+'/pb'+str(numer)+'/' + str(x) + '_large.png')
+    resp = s.get('https://www.pb.pl/konto/e-wydanie/' + str(rok) + '/pb' + numer.zfill(3) + '/' + str(x) + '_large.png')
     if resp.status_code == 404:
         eof = 1
     else:
@@ -32,13 +55,16 @@ while eof == 0:
         x = x + 1
 
 x = x - 1
-print "Obecne wydanie ma " + str(x) + " stron. Generuje PDFa"
+print("Obecne wydanie ma " + str(x) + " stron. Generuje PDFa")
 filelist = list(range(0, x))
-for i in range(1, x+1):
-    filelist[i-1] = str(i) + ".png"
+for i in range(1, x + 1):
+    filelist[i - 1] = str(i) + ".png"
 
-with open("pulsbiznesu "+str(numer)+" "+str(rok)+".pdf", "wb") as f:
-    f.write(img2pdf.convert(filelist))
+pdf = FPDF()
+for image in filelist:
+    pdf.add_page()
+    pdf.image(image, 0, 0, 210, 297)
+pdf.output("pulsbiznesu " + numer.zfill(3) + " " + str(rok) + ".pdf", "F")
 
 for filename in filelist:
     os.remove(filename)
